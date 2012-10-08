8a993544cfaf4951d6629feb2d37b05c85de8b62
Oliver Dressler <odressler@gmail.com>
2012-10-08 11:24:51 +0200

    changed licence and description

--- SequenceProcessing/SeqGUI.py
+++ SequenceProcessing/SeqGUI.py
@@ -1013,25 +1013,28 @@ class SeqGUI(wx.Frame):
             self.celltab.update_tree(self.top_cell)
                     
     def OnAbout(self, e):
-        description = """Sequence Processing 0.1 is a tool to model a pool of cells defined by specific sequences. 
-        It entails various methods to run the model and analyze the outcome.
-        
-        Start by loading an example sequence from a file, creating a cell, 
-        translating the sequence in this cell. Then run all cells for some rounds...
+        description = """Sequence Processing is a stochastic and discreet cell model.
+It was created to simplify the setup of a model, while still allowing for the creation
+of elaborate systems.
+I'm only coding since half a year, so every constructive feedback is very welcome!
         """
 
-        licence = '''Sequence Processing is free software: you can redistribute it and/or modify
-        it under the terms of the GNU General Public License as published by
-        the Free Software Foundation, either version 3 of the License, or
-        (at your option) any later version.
-
-        Sequence Processing is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
-
-        You should have received a copy of the GNU General Public License
-        along with Sequence Processing.  If not, see <http://www.gnu.org/licenses/>.
+        licence = '''Copyright (C) 2012 Oliver Dressler
+        
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+        associated documentation files (the "Software"), to deal in the Software without restriction, 
+        including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+        sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+        is furnished to do so, subject to the following conditions:
+        
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+        INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+        DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+        ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         '''
 
         info = wx.AboutDialogInfo()
@@ -1041,7 +1044,6 @@ class SeqGUI(wx.Frame):
         info.SetDescription(description)
         info.SetCopyright('(C) 2012 Oliver Dressler')
         info.SetLicence(licence)
-        info.AddDeveloper('Oliver Dressler')
         
         wx.AboutBox(info)
 
